816546a477dcd94e2b50d86c94268448c6016c42
SaraMotyka <saramotyka97@gmail.com>
2023-06-02 20:12:36 +0200

    added styles

--- src/index.js
+++ src/index.js
@@ -22,26 +22,35 @@ const PARAMS = {
 
 async function fetchImages(q, page, perPage) {
   try {
-    const response = await axios.get(
-      `${API_URL}?key=${API_KEY}&${PARAMS}&q=${query}&page=${page}&per_page=${perPage}`
-    );
-    if (!response.ok) {
-      if (response.status === 400) {
-        return [];
+    const response = await axios(
+      //`${API_URL}?key=${API_KEY}&${PARAMS}&q=${query}&page=${page}&per_page=${perPage}`
+      {
+        method: 'get',
+        baseURL: API_URL,
+        params: {
+          key: API_KEY,
+          image_type: 'photo',
+          orientation: 'horizontal',
+          safesearch: true,
+          per_page: perPage,
+          page,
+          q,
+        },
       }
-      return { error: response.status };
-    }
-    return response.data.json();
-  } catch (e) {
-    return { error: e.toString() };
+    );
+    console.log(response);
+    return response;
+  } catch (error) {
+    return error;
   }
 }
+
 function loading() {
   page += 1;
   simpleLightbox.destroy();
   q = searchForm.searchQuery.value;
   fetchImages(q, page, perPage).then(response => {
-    const galleryCard = response.hits
+    const galleryCard = data.hits
       .map(
         card =>
           `<a class="gallery-link" href="${card.largeImageURL}">
@@ -59,7 +68,7 @@ function loading() {
       .join('');
     gallery.insertAdjacentHTML('beforeend', galleryCard);
     simpleLightbox = new SimpleLightbox('.gallery').refresh();
-    const allPages = Math.ceil(response.totalHits / perPage);
+    const allPages = Math.ceil(data.totalHits / perPage);
 
     if (page >= allPages) {
       loadBtn.classList.add('hidden');
@@ -76,34 +85,97 @@ function createGallery(e) {
   gallery.innerHTML = '';
   loadBtn.classList.add('hidden');
 
-  fetchImages(q, page, perPage).then(response => {
-    if (response.totalHits === 0) {
-      Notiflix.Notify.failure(
-        'Sorry, there are no images matching your search query. Please try again.'
-      );
-    } else {
-      const galleryCards = response.hits
-        .map(
-          cards => `<a class="gallery-link" href="${cards.largeImageURL}">
-        <div class="photo-card">
-          <img class="gallery-photo" src="${cards.webFormatURL}" alt="${tags}" loading="lazy" />
+  fetchImages(q, page, perPage)
+    .then(response => {
+      console.log(response.data);
+      if (response.data.totalHits === 0) {
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      } else {
+        // const galleryCards = response.data.hits
+        //   .map(cards => {
+        //     return `<a class="gallery-link" href="${cards.largeImageURL}">
+        //       <div class="photo-card">
+        //         <img class="gallery-photo" src="${cards.webFormatURL}" alt="${tags}" loading="lazy" />
+        //         <div class="info">
+        //           <p class="info-item"><b>Likes</b>${cards.likes}</p>
+        //           <p class="info-item"><b>Views</b>${cards.views}</p>
+        //           <p class="info-item"><b>Comments</b>${cards.comments}</p>
+        //           <p class="info-item"><b>Downloads</b>${cards.downloads}</p>
+        //         </div>
+        //       </div>
+        //     </a>`;
+        //   })
+        //   .join('');
+        let galleryItem = [];
+        for (const image of response.data.hits) {
+          const {
+            webformatURL,
+            largeImageURL,
+            tags,
+            likes,
+            views,
+            comments,
+            downloads,
+          } = image;
+          galleryItem.push(`
+          <div class="photo-card">
+          <div class="gallery__pictureholder">
+          <a  href="${largeImageURL}">
+          <img class="gallery__picture" src="${webformatURL}" alt="${
+            tags + ' - from pixabay'
+          }" loading="lazy" />
+          </a>
+          </div>
           <div class="info">
-            <p class="info-item"><b>Likes</b>${cards.likes}</p>
-            <p class="info-item"><b>Views</b>${cards.views}</p>
-            <p class="info-item"><b>Comments</b>${cards.comments}</p>
-            <p class="info-item"><b>Downloads</b>${cards.downloads}</p>
+            <p class="info-item">
+              <b>Likes</b><span> ${likes}</span>
+            </p>
+            <p class="info-item">
+              <b>Views</b><span> ${views}</span>
+            </p>
+            <p class="info-item">
+              <b>Comments</b><span> ${comments}</span>
+            </p>
+            <p class="info-item">
+              <b>Downloads</b><span> ${downloads}</span>
+            </p>
           </div>
         </div>
-      </a>`
-        )
-        .join('');
-      gallery.insertAdjacentHTML('beforeend', galleryCards);
-      simpleLightbox = new SimpleLightbox('.gallery').refresh();
-      if (response.totalHits > 40) {
-        loading.classList.remove('hidden');
+          `);
+        }
+        let galleryToPublish = galleryItem.map(image => image).join(''); //building one string from array
+        // console.log(galleryCards);
+        gallery.insertAdjacentHTML('beforeend', galleryToPublish);
+        simpleLightbox = new SimpleLightbox('.gallery').refresh();
+        if (response.data.totalHits > 40) {
+          loading.classList.remove('hidden');
+        }
       }
-    }
-  });
+    })
+    .catch(error => {
+      console.log(error);
+      Notiflix.Notify.failure(`${error}`);
+      // const errorMn = error.toJSON();
+      // Notiflix.Notify.failure(`${errorMn}`);
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        Notiflix.Notify.failure(`${error.response.data}`);
+        Notiflix.Notify.failure(`${error.response.status}`);
+        Notiflix.Notify.failure(`${error.response.headers}`);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        Notiflix.Notify.failure(`${error.request}`);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        Notiflix.Notify.failure(`Error ${error.message}`);
+      }
+      Notiflix.Notify.failure(`${error.config}`);
+    });
 }
 
 searchForm.addEventListener('submit', createGallery);
