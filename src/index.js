ca87fb7f6db4c955b9abcd70afef9a206c7c053d
SaraMotyka <saramotyka97@gmail.com>
2023-06-02 16:26:15 +0200

    modified

--- src/index.js
+++ src/index.js
@@ -8,10 +8,9 @@ const gallery = document.querySelector('.gallery');
 const loadBtn = document.querySelector('.load-more');
 
 let page = 1;
-let query = searchForm.searchQuery.value;
 let simpleLightbox;
 let perPage = 40;
-let galleryCard;
+let q = '';
 
 const API_URL = 'https://pixabay.com/api/';
 const API_KEY = '36940182-0da281a971cf517379f0112d8';
@@ -21,7 +20,7 @@ const PARAMS = {
   safesearch: 'true',
 };
 
-async function fetchImages(query, page, perPage) {
+async function fetchImages(q, page, perPage) {
   try {
     const response = await axios.get(
       `${API_URL}?key=${API_KEY}&${PARAMS}&q=${query}&page=${page}&per_page=${perPage}`
@@ -32,88 +31,80 @@ async function fetchImages(query, page, perPage) {
       }
       return { error: response.status };
     }
-
-    const { hits: photos } = await response.json();
-
-    return photos;
+    return response.data.json();
   } catch (e) {
     return { error: e.toString() };
   }
 }
-
-function createGallery(query) {
-  const galleryCard = query
-    .map(card => {
-      const {
-        webFormatURL,
-        largeImageURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-      } = card;
-      return `<a class="gallery-link" href="${largeImageURL}">
-        <div class="photo-card">
-          <img class="gallery-photo" src="${webFormatURL}" alt="${tags}" loading="lazy" />
-          <div class="info">
-            <p class="info-item"><b>Likes</b>${likes}</p>
-            <p class="info-item"><b>Views</b>${views}</p>
-            <p class="info-item"><b>Comments</b>${comments}</p>
-            <p class="info-item"><b>Downloads</b>${downloads}</p>
-          </div>
-        </div>
-      </a>`;
-    })
-    .join('');
-  gallery.insertAdjacentHTML('beforeend', galleryCard);
-}
-
-loadBtn.addEventListener(
-  'click',
-  () => {
-    query = searchQuery.value;
-    page += 1;
-    fetchImages(query, page, perPage).then(query => {
-      let totalPages = query.totalHits / perPage;
-      renderGallery(query);
-      new SimpleLightbox('.gallery');
-      if (page >= totalPages) {
-        loadBtn.style.display = 'none';
-        Notiflix.Notify.info(
-          "We're sorry, but you've reached the end of search results."
-        );
-      }
-    });
-  },
-  true
-);
-
 function loading() {
   page += 1;
   simpleLightbox.destroy();
-  fetchImages(query, page, perPage)
-    .then(({ data }) => {
-      createGallery(data.hits);
-      simpleLightbox = new SimpleLightbox('.gallery').refresh();
-      const allPages = Math.ceil(data.totalHits / perPage);
+  q = searchForm.searchQuery.value;
+  fetchImages(q, page, perPage).then(response => {
+    const galleryCard = response.hits
+      .map(
+        card =>
+          `<a class="gallery-link" href="${card.largeImageURL}">
+        <div class="photo-card">
+          <img class="gallery-photo" src="${card.webFormatURL}" alt="${tags}" loading="lazy" />
+          <div class="info">
+            <p class="info-item"><b>Likes</b>${card.likes}</p>
+            <p class="info-item"><b>Views</b>${card.views}</p>
+            <p class="info-item"><b>Comments</b>${card.comments}</p>
+            <p class="info-item"><b>Downloads</b>${card.downloads}</p>
+          </div>
+        </div>
+      </a>`
+      )
+      .join('');
+    gallery.insertAdjacentHTML('beforeend', galleryCard);
+    simpleLightbox = new SimpleLightbox('.gallery').refresh();
+    const allPages = Math.ceil(response.totalHits / perPage);
 
-      if (page >= allPages) {
-        loadBtn.classList.add('hidden');
-        return Notiflix.Notify.failure(
-          "We're sorry, but you've reached the end of search results."
-        );
-      }
-    })
-    .catch(error => console.log(error));
+    if (page >= allPages) {
+      loadBtn.classList.add('hidden');
+      return Notiflix.Notify.failure(
+        "We're sorry, but you've reached the end of search results."
+      );
+    }
+  });
 }
-function handleSearchForm(e) {
+function createGallery(e) {
   e.preventDefault();
   page = 1;
-  query = e.currentTarget.searchQuery.value;
+  q = searchForm.searchQuery.value;
   gallery.innerHTML = '';
   loadBtn.classList.add('hidden');
+
+  fetchImages(q, page, perPage).then(response => {
+    if (response.totalHits === 0) {
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    } else {
+      const galleryCards = response.hits
+        .map(
+          cards => `<a class="gallery-link" href="${cards.largeImageURL}">
+        <div class="photo-card">
+          <img class="gallery-photo" src="${cards.webFormatURL}" alt="${tags}" loading="lazy" />
+          <div class="info">
+            <p class="info-item"><b>Likes</b>${cards.likes}</p>
+            <p class="info-item"><b>Views</b>${cards.views}</p>
+            <p class="info-item"><b>Comments</b>${cards.comments}</p>
+            <p class="info-item"><b>Downloads</b>${cards.downloads}</p>
+          </div>
+        </div>
+      </a>`
+        )
+        .join('');
+      gallery.insertAdjacentHTML('beforeend', galleryCards);
+      simpleLightbox = new SimpleLightbox('.gallery').refresh();
+      if (response.totalHits > 40) {
+        loading.classList.remove('hidden');
+      }
+    }
+  });
 }
 
-searchForm.addEventListener('submit', handleSearchForm);
+searchForm.addEventListener('submit', createGallery);
 loadBtn.addEventListener('click', loading);
